0ae876f78a8826e22e3d9dfbae353c7c87a9dc54
Augustoamrl <56563966+Augustoamrl@users.noreply.github.com>
2023-02-15 13:13:29 -0300

    feat: search bar

--- src/app/features/marvel/personagem/personagem-list/personagem-list.component.ts
+++ src/app/features/marvel/personagem/personagem-list/personagem-list.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personagem-list.component.scss']
 })
 export class PersonagemListComponent implements OnInit {
-
+  herois: any[] = [
+    { personagem: 'Homem-Aranha', series: 'O Espetacular Homem-Aranha', eventos: 'A Queda dos Vingadores' },
+    { personagem: 'Capitão América', series: 'Capitão América: O Primeiro Vingador', eventos: 'A Guerra Civil' },
+    { personagem: 'Thor', series: 'O Poderoso Thor', eventos: 'O Ragnarok' },
+    { personagem: 'Hulk', series: 'O Incrível Hulk', eventos: 'A Era de Ultron' },
+    { personagem: 'Viúva Negra', series: 'Viúva Negra', eventos: 'O Cerco' }
+  ];
+  nomeBusca: string = '';
+  paginaAtual: number = 1;
+  itensPorPagina: number = 2;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/features/marvel/personagem/personagem.module.ts
+++ src/app/features/marvel/personagem/personagem.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonagemRoutingModule } from './personagem-routing.module';
 import { PersonagemListComponent } from './personagem-list/personagem-list.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -10,6 +11,8 @@ import { PersonagemListComponent } from './personagem-list/personagem-list.compo
     imports: [
         CommonModule,
         PersonagemRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
     ],
 })
 export class PersonagemModule { }
